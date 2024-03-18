a21fbe4bde0a795561f43f017ce15473dd3fafd2
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-18 23:58:45 +0530

    update

---
+++ example1/script.js
@@ -0,0 +1,16 @@
+
+console.log(a);
+console.log(b);
+console.log(c);
+let a = 4
+var b = 5
+const c = 6
+console.log(a);
+console.log(b);
+console.log(c);
+
+function add() {
+    var s = 5
+}
+add()
+console.log(s);
