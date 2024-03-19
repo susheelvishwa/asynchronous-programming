071c93fbf1df189f84b77f43778be3719455161d
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-19 11:36:45 +0530

    practice asyncronous programming

---
+++ example2/script.js
@@ -0,0 +1,12 @@
+
+// Example 1
+
+console.log("code execuation start here");
+function hello() {
+    for (var i = 0; i <= 10; i++) {
+    
+    }
+    console.log("hello");
+}
+setTimeout(hello, 3000)
+console.log("code execuation end here");
---
+++ example3/script.js
@@ -0,0 +1,21 @@
+// example 2
+
+console.log("program start rom here");
+
+function multi() {
+    console.log("maruti speed is 7000ms");
+}
+function bus() {
+  console.log("bus speed is 1000ms");
+}
+function motercycle() {
+  console.log("motercycle speed is 3000ms");
+}
+setTimeout(multi, 7000)
+setTimeout(bus, 1000)
+setTimeout(motercycle, 3000)
+
+console.log("program ends here")
+
+
+
