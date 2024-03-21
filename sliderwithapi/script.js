001ce38a40eab2f83c7fa847a73b947e654970f8
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-22 00:44:41 +0530

    slider get the data from db.json then show the UI in the slider form

---
+++ sliderwithapi/script.js
@@ -0,0 +1,30 @@
+const slidesContainer = document.querySelector('.slides');
+let currentIndex = 0;
+
+// Fetch data from db.json
+fetch('db.json')
+  .then(response => response.json())
+  .then(data => {
+    data.product_data.forEach(product => {
+      const slideElement = document.createElement('div');
+      slideElement.classList.add('slide');
+      slideElement.classList.add("image");
+      slideElement.classList.add("title");
+      slideElement.classList.add("price");
+      slideElement.innerHTML = `
+        <img src="${product.image}" alt="${product.title}">
+        <div class="product-details">
+          <h3>${product.title}</h3>
+          <p>${product.price}</p>
+        </div>`;
+      slidesContainer.appendChild(slideElement);
+    });
+  });
+
+function changeSlide(direction) {
+  const slides = document.querySelectorAll('.slide');
+  const totalSlides = slides.length;
+  currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+  const offset = -currentIndex * 100;
+  slidesContainer.style.transform = `translateX(${offset}%)`;
+}
