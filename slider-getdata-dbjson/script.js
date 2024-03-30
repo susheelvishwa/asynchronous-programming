c98eed270a095f34eed7959f8a88824ddf5af21a
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-30 22:41:54 +0530

    slider update

--- slider-getdata-dbjson/script.js
+++ slider-getdata-dbjson/script.js
@@ -1,28 +1,7 @@
-const slidesContainer = document.querySelector(".slides-1"); 
-const prev = document.querySelector(".prev-1")
+const slidesContainer = document.querySelector(".slides-1");
+const prev = document.querySelector(".prev-1");
 const next = document.querySelector(".next-1");
 let currentIndex = 0;
-
-
-prev.addEventListener("click", handlePrev)
-next.addEventListener("click", handleNext);
-
-var counter = 0
-function handlePrev() {
-  counter--;
-  if (counter < 0) {
-    counter = slide.length - 1;
-  }
-  slideimage();
-}
-
-function handleNext() {
-  counter++;
-  if (counter >= slide.length) {
-    counter = 0;
-  }
-  slideimage();
-}
 
 fetch("db.json")
   .then((response) => response.json())
@@ -45,7 +24,7 @@ fetch("db.json")
 
 function changeSlide(direction) {
   const slides = document.querySelectorAll(".slide");
-  const totalSlides = slides.length;
+  const totalSlides = slides.length-1;
   currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
   const offset = -currentIndex * 100;
   slidesContainer.style.transform = `translateX(${offset}%)`;
